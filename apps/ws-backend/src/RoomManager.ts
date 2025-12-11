982563db3eddf32721028a28c5592c15ccefc8b2
Prathamesh0901 <prathameshmane3622@gmail.com>
2025-12-11 12:03:50 +0530

    added redis for non sticky connections

--- apps/ws-backend/src/RoomManager.ts
+++ apps/ws-backend/src/RoomManager.ts
@@ -1,13 +1,20 @@
+import { createClient, type RedisClientType } from 'redis';
 import { ServerMessage } from "@repo/common/messageTypes";
 import WebSocket from 'ws';
 
 export class RoomManager {
     private static instance: RoomManager;
+    private subscribeClient: RedisClientType;
+    private publishClient: RedisClientType;
     private rooms: Map<string, WebSocket[]>;
     private sockets: Map<WebSocket, string>;
     private roomId: Map<string, string>;
 
     private constructor() {
+        this.publishClient = createClient();
+        this.publishClient.connect();
+        this.subscribeClient = this.publishClient.duplicate();
+        this.subscribeClient.connect();
         this.rooms = new Map();
         this.sockets = new Map();
         this.roomId = new Map();
@@ -20,12 +27,28 @@ export class RoomManager {
     }
 
     joinRoom (roomId: string, ws: WebSocket, userId: string) {
-        const u = this.rooms.get(roomId) || [];
-        this.rooms.set(roomId, [...u, ws]);
+        if(!this.rooms.has(roomId)) 
+            this.rooms.set(roomId, []);
+        this.rooms.get(roomId)?.push(ws);
+        if(this.rooms.get(roomId)?.length === 1) {
+            this.subscribeClient.subscribe(roomId, (message) => {
+                this.forwardMessageToUser(roomId, message);
+            });
+            console.log('Subscribed to this room', roomId);
+        }
         this.sockets.set(ws, userId);
     }
 
+    forwardMessageToUser(roomId: string, message: string) {
+        this.rooms.get(roomId)?.forEach(ws => {
+            if(ws?.readyState === WebSocket.OPEN) {
+                ws.send(message);
+            }
+        })
+    }
+
     broadcast (roomId: string, message: ServerMessage, senderWS: WebSocket) {
+        this.publishClient.publish(roomId, JSON.stringify(message));
         this.rooms.get(roomId)?.forEach(user => {
             if(user && user !== senderWS && user.readyState === WebSocket.OPEN) {
                 user.send(JSON.stringify(message));
@@ -34,7 +57,12 @@ export class RoomManager {
     }
 
     leaveRoom (roomId: string, ws: WebSocket) {
-        this.rooms.set(roomId, this.rooms.get(roomId)?.filter(user => user !== ws) || []);
+        this.rooms.set(roomId, this.rooms.get(roomId)?.filter(s => s !== ws) || []);
+        
+        if(this.rooms.get(roomId)?.length === 0) {
+            this.subscribeClient.unsubscribe(roomId);
+            console.log('Unsubscribed to this channel', roomId);
+        }
         this.sockets.delete(ws);
         ws.close();
     }
