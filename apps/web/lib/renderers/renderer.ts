b986f2839739bf6ab00389b1b4cb2b33921f32da
Prathamesh0901 <prathameshmane3622@gmail.com>
2025-12-26 23:27:33 +0530

    renderer complete

--- apps/web/lib/renderers/renderer.ts
+++ apps/web/lib/renderers/renderer.ts
@@ -1,65 +1,62 @@
 import { Element } from "../types/types";
 
-export function renderRectangle (ctx: CanvasRenderingContext2D, element: Element) {
+export function renderRectangle(ctx: CanvasRenderingContext2D, element: Element) {
     if (element.type !== 'RECTANGLE') return;
 
     ctx.strokeStyle = 'rgb(255, 255, 255)';
-    ctx.lineWidth   = 1;
-
-    const {x, y, w, h} = element.data;
-
+    ctx.lineWidth = 1;
+    const { x, y, w, h } = element.data;
     ctx.strokeRect(x, y, w, h);
 }
 
-export function renderEllipse (ctx: CanvasRenderingContext2D, element: Element) {
+export function renderEllipse(ctx: CanvasRenderingContext2D, element: Element) {
     if (element.type !== 'ELLIPSE') return;
 
     ctx.strokeStyle = 'rgb(255, 255, 255)';
-    ctx.lineWidth   = 1;
+    ctx.lineWidth = 1;
 
-    const {x, y, rX, rY} = element.data;
-
+    const { x, y, rX, rY } = element.data;
     ctx.beginPath();
-    ctx.ellipse(x, y, Math.abs(rX), Math.abs(rY), 0, 0, 2*Math.PI);
+    ctx.ellipse(x, y, Math.abs(rX), Math.abs(rY), 0, 0, 2 * Math.PI);
     ctx.stroke();
 }
 
-export function renderArrow (ctx: CanvasRenderingContext2D, element: Element) {
+export function renderArrow(ctx: CanvasRenderingContext2D, element: Element) {
     if (element.type !== 'ARROW') return;
 
     ctx.strokeStyle = 'rgb(255, 255, 255)';
-    ctx.lineWidth   = 1;
+    ctx.lineWidth = 1;
 
-    const {sX, sY, eX, eY, headlen} = element.data;
-
+    const { sX, sY, eX, eY, headlen } = element.data;
+    
     ctx.beginPath();
     ctx.moveTo(sX, sY);
     ctx.lineTo(eX, eY);
     ctx.stroke();
 
-    const angle = Math.atan2(eY - sY, eX - sX);
+    const a = Math.atan2(eY - sY, eX - sX);
 
     ctx.beginPath();
     ctx.moveTo(eX, eY);
     ctx.lineTo(
-        eX - headlen * Math.cos(angle - 0.3),
-        eY - headlen * Math.sin(angle - 0.3),
+        eX - headlen * Math.cos(a - 0.3),
+        eY - headlen * Math.sin(a - 0.3),
     );
     ctx.moveTo(eX, eY);
     ctx.lineTo(
-        eX - headlen * Math.cos(angle + 0.3),
-        eY - headlen * Math.sin(angle + 0.3),
+        eX - headlen * Math.cos(a + 0.3),
+        eY - headlen * Math.sin(a + 0.3),
     );
     ctx.stroke();
 }
 
-export function renderLine (ctx: CanvasRenderingContext2D, element: Element) {
+export function renderLine(ctx: CanvasRenderingContext2D, element: Element) {
     if (element.type !== 'LINE') return;
 
     ctx.strokeStyle = 'rgb(255, 255, 255)';
-    ctx.lineWidth   = 1;
+    ctx.lineWidth = 1;
 
-    const {sX, sY, eX, eY} = element.data;
+    const { sX, sY, eX, eY } = element.data;
 
     ctx.beginPath();
     ctx.moveTo(sX, sY);
@@ -67,15 +64,15 @@ export function renderLine (ctx: CanvasRenderingContext2D, element: Element) {
     ctx.stroke();
 }
 
-export function renderPencil (ctx: CanvasRenderingContext2D, element: Element) {
+export function renderPencil(ctx: CanvasRenderingContext2D, element: Element) {
     if (element.type !== 'PENCIL') return;
-    
-    ctx.strokeStyle = 'rgb(255, 255, 255)';
-    ctx.lineWidth   = 1;
-    ctx.lineCap     = 'round';
-    ctx.lineJoin    = 'round';
 
-    const points = element.data.points;
+    ctx.strokeStyle = 'rgb(255, 255, 255)';
+    ctx.lineWidth = 1;
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+
+    const { points } = element.data;
 
     ctx.beginPath();
     points.forEach((p, i) => {
@@ -89,30 +86,47 @@ export function renderPencil (ctx: CanvasRenderingContext2D, element: Element) {
     ctx.stroke();
 }
 
-export function renderText (ctx: CanvasRenderingContext2D, element: Element) {
+export function renderText(ctx: CanvasRenderingContext2D, element: Element) {
     if (element.type !== 'TEXT') return;
-    const {x, y, text, fontSize, fontFamily = 'Arial', angle} = element.data;
-
-    const lines = text.split('\n');
-    let w = 0, h = 0;
-
-    ctx.save();
+    const { x, y, text, fontSize, fontFamily = 'Arial', currWidth, lineHeight } = element.data;
     
-    for (const line of lines) {
-        ctx.font = `${fontSize}px ${fontFamily}`;
-        ctx.fillStyle = 'rgb(255, 255, 255)';
-        ctx.textBaseline = 'top';
+    ctx.font = `${fontSize}px ${fontFamily}`;
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    ctx.lineWidth = 1;
+    ctx.textBaseline = 'top';
+    
+    const resultLines: string[] = [];
+    const paragraphs = text.split("\n");
+
+    paragraphs.forEach(paragraph => {
+
+        const tokens = paragraph.match(/\S+\s*/g) || [];
+
+        let currentLine = "";
+
+        for (const token of tokens) {
+            const testLine = currentLine + token;
+            const measure = ctx.measureText(testLine);
+            const width = measure.width;
+            if (width > currWidth && currentLine !== "") {
+                resultLines.push(currentLine);
+                currentLine = token;
+            } else {
+                currentLine = testLine;
+            }
+        }
         
-        const metrics = ctx.measureText(line);
-        w = Math.max(w, metrics.width);
-        h += metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent + 5;
-    }
+        if (currentLine) {
+            resultLines.push(currentLine);
+        }
+    })
+
+    element.data.lineCount = resultLines.length;
     
     let offset = y;
-    
-    for(const line of lines) {
+
+    resultLines.forEach((line) => {
         ctx.fillText(line, x, offset);
-        offset += h;
-    }
-    ctx.restore();
+        offset += fontSize + lineHeight;
+    })
 }
