6540a1ee74fa5d9a94bdf1c75e2985d30fe0b568
Prathamesh0901 <prathameshmane3622@gmail.com>
2026-01-01 23:35:07 +0530

    new fetch functions

--- apps/web/lib/utils/fetch.ts
+++ apps/web/lib/utils/fetch.ts
@@ -1,7 +1,7 @@
 import { HTTP_BACKEND_URL } from "../../config";
 import { Element } from "../types/types";
 
-export async function getExisitingElements (slug: string): Promise<Element[]> {
+export async function getExisitingElements (slug: string): Promise<{roomId: string, elements: Element[]}> {
     try {
         const token = JSON.parse(localStorage.getItem('draw-app-data') || '');
         const data = await fetch(`${HTTP_BACKEND_URL}/elements/${slug}`, {
@@ -11,14 +11,107 @@ export async function getExisitingElements (slug: string): Promise<Element[]> {
                 'authorization': `Bearer ${token}`
             }
         });
-        console.log(data);
-        if(data.status !== 200) return [];
-        const messages = await data.json();
-        const shapes = messages.elements;
-        
-        return shapes;
+        if(data.status !== 200) return {
+            roomId: '',
+            elements: []
+        };
+        const message = await data.json();
+        return message;
     } catch (error) {
         console.log('Error fetching existing shapes:', error);
-        return [];
+        return {
+            roomId: '',
+            elements: []
+        };
+    } 
+}
+
+export async function getInviteToken (slug: string) {
+    try {
+        const token = JSON.parse(localStorage.getItem('draw-app-data') || '');
+        const data = await fetch(`${HTTP_BACKEND_URL}/invite/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                slug
+            })
+        });
+
+        if(data.status !== 200) return '';
+        const message = await data.json();
+        const inviteToken = message.token;
+        console.log(inviteToken);
+        return inviteToken;
+    } catch (error) {
+        console.log('Error fetching existing shapes:', error);
+        return '';
+    } 
+}
+
+export async function verifyToken (inviteToken: string) {
+    try {
+        const data = await fetch(`${HTTP_BACKEND_URL}/invite/verify`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                token: inviteToken
+            })
+        });
+
+        if(data.status !== 200) return false;
+        const message = await data.json();
+        console.log(message);
+        return message;
+    } catch (error) {
+        console.log('Error verifying token:', error);
+        return false;
+    } 
+}
+
+export async function acceptToken (inviteToken: string) {
+    try {
+        const token = JSON.parse(localStorage.getItem('draw-app-data') || '');
+        const data = await fetch(`${HTTP_BACKEND_URL}/invite/accept`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                token: inviteToken
+            })
+        });
+
+        if(data.status !== 200) return false;
+        const message = await data.json();
+        return message;
+    } catch (error) {
+        console.log('Error verifying token:', error);
+        return false;
+    } 
+}
+
+export async function getRoomId (slug: string) {
+    try {
+        const token = JSON.parse(localStorage.getItem('draw-app-data') || '');
+        const data = await fetch(`${HTTP_BACKEND_URL}/rooms/${slug}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `Bearer ${token}`
+            }
+        });
+
+        if(data.status !== 200) return false;
+        const message = await data.json();
+        return message;
+    } catch (error) {
+        console.log('Error verifying token:', error);
+        return false;
     } 
 }
