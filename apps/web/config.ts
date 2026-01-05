5270ddfe6e84dfc483eecd605c9f07b0b70fba7d
Prathamesh0901 <prathameshmane3622@gmail.com>
2026-01-05 23:15:53 +0530

    added env urls

--- apps/web/config.ts
+++ apps/web/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_BACKEND_URL = 'http://localhost:3001';
-export const WS_BACKEND_URL = 'ws://localhost:3002';
+export const HTTP_BACKEND_URL = process.env.NEXT_PUBLIC_HTTP_BACKEND_URL || 'http://localhost:3001';
+export const WS_BACKEND_URL =  process.env.NEXT_PUBLIC_WS_BACKEND_URL || 'ws://localhost:3002';
